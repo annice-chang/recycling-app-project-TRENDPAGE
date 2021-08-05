01c653c540164477498e030fe497782c0eb78d2f
Annice <anniceychang2005@gmail.com>
2021-08-05 12:13:46 -0400

    updated trend page

--- recycling-app-project/BatteriesViewController.swift
+++ recycling-app-project/BatteriesViewController.swift
@@ -15,15 +15,13 @@ class BatteriesViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let trendController = segue.destination as! TrendViewController
+        trendController.batteriesRecycled += 1
     }
-    */
-
+    
+    @IBAction func batteriesRecycledButton(_ sender: Any) {
+        
+    }
+    
 }
--- recycling-app-project/TrendViewController.swift
+++ recycling-app-project/TrendViewController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class TrendViewController: UIViewController {
 
+    @IBOutlet weak var batteriesTrendCount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+
+    var batteriesRecycled = 0
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
